6d15992bb120e30201aa596ddb834f4f713921d6
pedrohpsantos <phps2014.phps@gmail.com>
2025-10-20 23:04:09 -0300

    add project euler folder

---
+++ project_euler/zero_problem.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define ll long long
+#define limit 800000
+int main()
+{
+    ll sum = 0;
+    for (int i = 1; i <= limit; i += 2)
+    {
+        sum += (ll)i * (ll)i;
+    }
+    printf("Sum of all odd squares = %lld\n", sum);
+    return 0;
+}
