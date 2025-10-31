8c55c2011a7978a2298d2c826fecf6f621b20cbe
pedrohpsantos <phps2014.phps@gmail.com>
2025-10-31 08:53:21 -0300

    organização e adicionando e1066.c

---
+++ beecrowd/iniciante/repeticao/e1066.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int par = 0, imp = 0, pos = 0, neg = 0, num;
+    for (int i = 0; i < 5; i++)
+    {
+        scanf("%d", &num);
+        if (num % 2 == 0)
+            par++;
+        else
+            imp++;
+        if (num < 0)
+            neg++;
+        else if (num > 0)
+            pos++;
+    }
+    printf("%d valor(es) par(es)\n", par);
+    printf("%d valor(es) impar(es)\n", imp);
+    printf("%d valor(es) positivo(s)\n", pos);
+    printf("%d valor(es) negativo(s)\n", neg);
+
+    return 0;
+}
