583e5cf93b47091be67d5316a9081cf30d5c2d89
pedrohpsantos <phps2014.phps@gmail.com>
2025-11-11 13:40:19 -0300

    adicionando e1079.c

---
+++ beecrowd/iniciante/repeticao/e1079.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        double a, b, c, media;
+        scanf("%lf %lf %lf", &a, &b, &c);
+        media = (a * 2 + b * 3 + c * 5) / 10;
+        printf("%.1lf\n", media);
+    }
+    return 0;
+}
